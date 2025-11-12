3d05eda58ffb25092fd338d3d98d832e425747f6
garv thakre <garvthakre0@gmail.com>
2025-11-12 11:37:02 +0530

    fix auth

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+// server/server.js - UPDATED with auto admin creation
 require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/config/database');
 const cronService = require('./src/services/cronService');
+const createDefaultAdmin = require('./src/utils/createDefaultAdmin');
 const logger = require('./src/utils/logger');
 
 const PORT = process.env.PORT || 5000;
@@ -11,6 +13,9 @@ const startServer = async () => {
   try {
     // Connect to database
     await connectDB();
+
+    // Create default admin if doesn't exist
+    await createDefaultAdmin();
 
     // Start cron jobs
     cronService.startDailyReset();
